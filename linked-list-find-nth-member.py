d9a43d443f0eb8da6e770377645172aa099caf75
Annibaldi <mannibaldi@tmwsystems.com>
2019-03-20 16:43:58 -0400

    Implement getting in one pass; fix two-pass due to getting clearer requirement

--- linked-list-find-nth-member.py
+++ linked-list-find-nth-member.py
@@ -1,6 +1,10 @@
 # Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list.
 # From Laakman-McDowell, "Cracking the Code Interview", 6th edition, problem 2.2, p. 94
 
+# Additional notes: Note that for this solution, we have defined k such that passing in k = 1 would return the last element, k
+# 2 would return to the second to last element, and so on. It is equally acceptable to define k such that k
+# = e would return the last element.
+
 import sys
 
 class Node:
@@ -32,11 +36,23 @@ class LinkedList:
     def traverseNItems(self, size, n):
         current = self.head
         count = 0
-        while current.next != None and count < size - n:
+        while current.next != None and count < size - n - 1:
             print("Current data is {}".format(current.data))
             current = current.next
             count += 1
-        return current.data            
+        return current.data     
+
+    def traverseNItemsOnePass(self, n):
+        current = self.head
+        ptr2 = self.head
+        count = 0
+        while current.next != None:
+            count += 1
+            if count > n:
+                ptr2 = ptr2.next
+            current = current.next
+        print(ptr2.data)
+        return ptr2.data
 
 def main():
     if len(sys.argv) < 2:
@@ -63,6 +79,10 @@ def main():
 
     print("Linked list size is {}".format(size))
     foundData = lnklst.traverseNItems(size, n)
+    print("Found data is {}".format(foundData))
+
+    print("Linkus listus traversus countus one passus")
+    foundData = lnklst.traverseNItemsOnePass(n)
     print("Found data is {}".format(foundData))
 
 if __name__ == '__main__':
